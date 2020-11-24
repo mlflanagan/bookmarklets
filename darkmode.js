ee778d50efbf2dcb601678c8354104ce9edc2216
mlflanagan <72321981+mlflanagan@users.noreply.github.com>
2020-11-24 11:04:58 -0500

    Create darkmode.js

---
+++ darkmode.js
@@ -0,0 +1,11 @@
+javascript: (
+    function() {
+        var s = document.createElement('style');
+        s.textContent = '* {
+            background-color: #000 !important;
+            color:#5e5 !important;
+            /* font-size:larger !important; */
+        }';
+        document.head.appendChild(s);
+    }
+)()
